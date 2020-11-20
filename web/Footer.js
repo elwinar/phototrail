5f332a48dbd379cd3d0147d1c501d52b76b443ca
Romain Baugue <romain.baugue@elwinar.com>
2020-11-21 00:21:35 +0100

    Give some love to the Footer

--- web/Footer.js
+++ web/Footer.js
@@ -1,13 +1,11 @@
 import React from "react";
-import "./App.scss";
+import styles from "./Footer.scss";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p>
-        For documentation, issues, see the{" "}
-        <a href="https://github.com/elwinar/phototrail">repository</a>.
-      </p>
-    </footer>
+    <p className={styles.Footer}>
+      For documentation, issues, see the{" "}
+      <a href="https://github.com/elwinar/phototrail">repository</a>.
+    </p>
   );
 }
