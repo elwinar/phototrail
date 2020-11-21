5a3f0fb8411b41cb4cd5270f51b29f7f417b4e75
Nicolas Missika <nmissika@synthesio.com>
2020-11-21 14:01:06 +0100

    change a few colors and first draft for comment

--- web/Card.js
+++ web/Card.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./api";
 import styles from "./Card.scss";
 
 export function Card({ post, onLike }) {
+  const [showComments, setShowComments] = useState(false);
+  const [comment, setComment] = useState("");
+
   const isLikedByMe = Boolean(
     (post.likes || []).find((l) => l.user_id === document.session.user_id)
   );
@@ -30,7 +34,31 @@ export function Card({ post, onLike }) {
         <button className={styles.Likes} onClick={() => onLike(post.id)}>
           <span className={isLikedByMe ? styles.Liked : ""}>&#9829;</span> {likes}
         </button>
+        {post.comments && (
+          <button
+            className={styles.CommentsButton}
+            onClick={() => setShowComments(!showComments)}
+          >
+            Comments
+          </button>
+        )}
       </figcaption>
+      {showComments && <section>comments here</section>}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          api.createComment(post.id, comment)
+        }}
+      >
+        <input
+          type="text"
+          name="comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          className={styles.Input}
+          placeholder="Your comment..."
+        />
+      </form>
     </figure>
   );
 }
