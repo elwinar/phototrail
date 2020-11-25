2ee67e94e97a5777ee0edd2b45b9ae6a89b1aae2
Romain Baugue <romain.baugue@elwinar.com>
2020-11-25 23:30:20 +0100

    Use the full datetime in cards

--- web/Card.js
+++ web/Card.js
@@ -10,7 +10,7 @@ export function Card({ post, onLike, onComment, onDeleteComment, onDeletePost })
 
   const isMine = post.user_id === document.session.user_id;
   const likes = (post.likes && post.likes.length.toLocaleString()) || "0";
-  const date = new Date(post.created_at).toLocaleDateString();
+  const date = new Date(post.created_at).toLocaleString();
 
   return (
     <figure className={styles.Card}>
@@ -29,10 +29,11 @@ export function Card({ post, onLike, onComment, onDeleteComment, onDeletePost })
         <p className={styles.Header}>
           <span>
             <span className={styles.Username}>{post.user_name}</span>
-            {
-              isMine &&
-              <button onClick={() => onDeletePost(post.id)} className={styles.DeletePostButton}>Delete post</button>
-            }
+            {isMine && (
+              <button onClick={() => onDeletePost(post.id)} className={styles.DeletePostButton}>
+                Delete post
+              </button>
+            )}
           </span>
           <span className={styles.Date}>{date}</span>
         </p>
@@ -47,12 +48,15 @@ export function Card({ post, onLike, onComment, onDeleteComment, onDeletePost })
               className={styles.CommentsButton}
               onClick={() => setShowComments(!showComments)}
             >
-              Comments ({post.comments.length.toLocaleString()}) {showComments ? <span>&#9652;</span> : <span>&#9662;</span>}
+              Comments ({post.comments.length.toLocaleString()}){" "}
+              {showComments ? <span>&#9652;</span> : <span>&#9662;</span>}
             </button>
           )}
         </footer>
       </figcaption>
-      {showComments && <Comments comments={post.comments} onDeleteComment={onDeleteComment} postId={post.id} />}
+      {showComments && (
+        <Comments comments={post.comments} onDeleteComment={onDeleteComment} postId={post.id} />
+      )}
       <CommentForm onComment={onComment} postId={post.id} setShowComments={setShowComments} />
     </figure>
   );
@@ -63,48 +67,60 @@ function Comments({ comments, onDeleteComment, postId }) {
     return null;
   }
 
-  return <section>
-    <ul className={styles.Comments}>{
-      comments.map(c => {
-        const commentDate = new Date(c.created_at).toLocaleDateString();
-        const isMine = document.session.user_id === c.user_id;
+  return (
+    <section>
+      <ul className={styles.Comments}>
+        {comments.map((c) => {
+          const commentDate = new Date(c.created_at).toLocaleDateString();
+          const isMine = document.session.user_id === c.user_id;
 
-        return <li key={c.id}>
-          <p className={styles.CommentInfo}>
-            <span>
-              <span className={styles.CommentDate}>{commentDate}</span>
-              <span className={styles.CommentUserName}>{c.user_name}</span>
-            </span>
-            {isMine && <button type="button" onClick={() => onDeleteComment(postId, c.id)} className={styles.DeleteComment}>Delete</button>}
-          </p>
-          <p className={`${styles.Comment} ${isMine ? styles.Mine : ''}`}>{c.text}</p>
-        </li>
-      })
-    }
-    </ul>
-  </section>
+          return (
+            <li key={c.id}>
+              <p className={styles.CommentInfo}>
+                <span>
+                  <span className={styles.CommentDate}>{commentDate}</span>
+                  <span className={styles.CommentUserName}>{c.user_name}</span>
+                </span>
+                {isMine && (
+                  <button
+                    type="button"
+                    onClick={() => onDeleteComment(postId, c.id)}
+                    className={styles.DeleteComment}
+                  >
+                    Delete
+                  </button>
+                )}
+              </p>
+              <p className={`${styles.Comment} ${isMine ? styles.Mine : ""}`}>{c.text}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
-
 
 function CommentForm({ onComment, postId, setShowComments }) {
   const [comment, setComment] = useState("");
 
-  return <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      setShowComments(true);
-      onComment(postId, comment).then(() => {
-        setComment("");
-      })
-    }}
-  >
-    <input
-      type="text"
-      name="comment"
-      value={comment}
-      onChange={(e) => setComment(e.target.value)}
-      className={styles.Input}
-      placeholder="Your comment..."
-    />
-  </form>
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setShowComments(true);
+        onComment(postId, comment).then(() => {
+          setComment("");
+        });
+      }}
+    >
+      <input
+        type="text"
+        name="comment"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        className={styles.Input}
+        placeholder="Your comment..."
+      />
+    </form>
+  );
 }
