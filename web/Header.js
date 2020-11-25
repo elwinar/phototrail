4cb2a61e11fcecf67c9609e0a2c52c8ec7fe8ada
Romain Baugue <romain.baugue@elwinar.com>
2020-11-25 23:30:36 +0100

    Autodetect logo presence

--- web/Header.js
+++ web/Header.js
@@ -2,9 +2,28 @@ import React from "react";
 import styles from "./Header.scss";
 
 export default function Header() {
+  const logoPath = `${document.config.baseURL}/images/logo.svg`;
+
+  const [hasLogo, setHasLogo] = React.useState(false);
+
+  React.useEffect(function lookupLogo() {
+    new Promise((resolve) => {
+      const img = new Image();
+      img.onload = function () {
+        resolve(true);
+      };
+      img.onerror = function () {
+        resolve(false);
+      };
+      img.src = logoPath;
+    }).then((res) => {
+      setHasLogo(res);
+    });
+  }, []);
+
   return (
     <header className={styles.Header}>
-      <h1>Phototrail</h1>
+      {hasLogo ? <img className={styles.Logo} src={logoPath} /> : <h1>Phototrail</h1>}
     </header>
   );
 }
